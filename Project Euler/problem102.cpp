3f3317767d0bf4ffcce4e960c9043cccd5742163
Basim Khajwal <basimkhajwal@gmail.com>
2016-11-04 23:07:19 +0000

    Entered correct solution for Project Euler 102

--- Project Euler/problem102.cpp
+++ Project Euler/problem102.cpp
@@ -1,30 +1,36 @@
+/* Correct answer */
 #include <cstdio>
 using namespace std;
 
-bool hasLeft(int x1, int y1, int x2, int y2) {
-    if (x1 > 0 && x2)
+int crossProduct(int x1, int y1, int x2, int y2) {
+    return x1*y2 - y1*x2;
 }
 
-//
-//  Use the cross product to determine which
-//  side of the triangle each point is opn
-//
+int sign(int x) {
+    if (x < 0) return -1;
+    if (x > 0) return 1;
+    return 0;
+}
 
+int x[3], y[3];
+
+inline bool hasOrigin() {
+    int a = sign(crossProduct(x[1] - x[0], y[1] - y[0], -x[0], -y[0]));
+    int b = sign(crossProduct(x[2] - x[1], y[2] - y[1], -x[1], -y[1]));
+    int c = sign(crossProduct(x[0] - x[2], y[0] - y[2], -x[2], -y[2]));
+    return a == b && b == c;
+}
 
 int main () {
-    
-    int x[3], y[3];
-    
     FILE *f = fopen("p102_triangles.txt", "r");
+    int count = 0;
     
     for (int i = 0; i < 1000; i++) {
         fscanf(f, "%d,%d,%d,%d,%d,%d", &x[0], &y[0], &x[1], &y[1], &x[2], &y[2]);
-        
-        // Check left
-        
+        if (hasOrigin()) count++;
     }
     
-    
+    printf("Total: %d\n", count);
     fclose(f);
     
     return 0;
