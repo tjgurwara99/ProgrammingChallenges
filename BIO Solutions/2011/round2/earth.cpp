e62bd75af8aea9b1e7ff274b5f8de2cf496695b0
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-08 20:01:13 +0000

    Finally did itgit add *!

--- BIO Solutions/2011/round2/earth.cpp
+++ BIO Solutions/2011/round2/earth.cpp
@@ -7,59 +7,24 @@ int N, M;
 pair<string,int> digitWords[10];
 int tenPow[10];
 
-int digitCount(int d) {
-    
-    if (d == 0) return 1;
-    
-    int totalCount = 0;
-    int dc = 1;
-    
-    while (tenPow[dc] <= N) {
-        totalCount += tenPow[dc-1];
-        dc++;
-    }
-    dc--;
-    
-    int fd = N / tenPow[dc];
-    if (fd == d) {
-        totalCount += (N % tenPow[dc]) + 1;
-    } else if (fd > d) {
-        totalCount += tenPow[dc];
-    }
-    
-    return totalCount;
-}
-
-int countPrefix(int number, int ans) {
-    
+int countPrefix(int ans) {
     if (ans == 0) return 1;
     
-    int dc = 0;
-    int totalCount = ans > number ? 0 : 1; // include ans
+    int totalCount = ans > N ? 0 : 1;
     
-    while (ans*10 <= number) {
+    for (int d = 0; ans*10 <= N; d++) {
         ans *= 10;
-        dc++;
-        totalCount += min(tenPow[dc], number-ans+1);
+        totalCount += min(tenPow[d+1], N-ans+1);
     }
     
     return totalCount;
-}
-
-int getNext(int n, int digit) {
-    int i = 0;
-    while (tenPow[i+1] <= n) i++;
-    
-    if (digit == n) return n % tenPow[i];
-    if (digit < n) return tenPow[i]-1;
-    
-    return i == 0 ? 0 : tenPow[i-1];
 }
 
 int main() {
     tenPow[0] = 1;
     for (int i = 1; i < 10; i++) tenPow[i] = 10*tenPow[i-1];
     
+    cin >> N >> M;
     for (int i = 0; i < 10; i++) {
         cin >> digitWords[i].first;
         digitWords[i].second = i;
@@ -68,34 +33,21 @@ int main() {
     
     int ans = 0;
     int idx = M;
-    bool first = true;
     
-    for (int i = 0; i < 10; i++) {
-        
-    }
-    
-    
-    int currentN = N;
-    
-    while (idx) {
+    do {
         idx--;
-        
-        int i;
-        for (i = 0; i < 10; i++) {
-            if (digitWords[i].second == 0 && first) continue;
+        for (int i = 0; i < 10; i++) {
+            int d = digitWords[i].second;
+            int count = countPrefix(ans*10+d);
             
-            int dc = digitCount(digitWords[i].second, currentN);
-            if (idx >= dc) {
-                idx -= dc;
+            if (idx >= count) {
+                idx -= count;
             } else {
-                ans = ans*10+digitWords[i].second;
+                ans = ans*10+d;
                 break;
             }
         }
-        
-        currentN = getNext(currentN, i);
-        first = false;
-    }
+    } while (idx);
     
     cout << ans << endl;
     
