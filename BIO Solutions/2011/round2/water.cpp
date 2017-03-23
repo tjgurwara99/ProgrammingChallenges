7d0acb8bfda01a7def50a00ba9b1f598871a7e8b
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-23 09:57:31 +0000

    Finished DP solution - now O(n^2)

--- BIO Solutions/2011/round2/water.cpp
+++ BIO Solutions/2011/round2/water.cpp
@@ -1,10 +1,13 @@
 #include <cstdio>
+#include <algorithm>
 using namespace std;
 
 int R, C;
 bool grid[257][257];
+int DP[257][257];
 
-void readInput() {
+int main() {
+
     char temp[300];
     scanf("%d %d", &R, &C);
 
@@ -12,16 +15,18 @@ void readInput() {
         scanf("%s", temp);
         for (int c = 0; c < C; c++) grid[r][c] = temp[c] == 'S';
     }
-}
-
-int DP[257][257][257];
-
-int main() {
-
-    readInput();
 
     int totalCount = 0;
-
+    
+    for (int r = R-1; r >= 0; r--) {
+        for (int c = C-1; c >= 0; c--) {
+            if (!grid[r][c]) continue;
+            DP[r][c] = 1 + min(DP[r+1][c], min(DP[r][c+1], DP[r+1][c+1]));
+            totalCount += DP[r][c]-1;
+        }
+    }
+    
+    printf("%d\n", totalCount);
 
     return 0;
 }
