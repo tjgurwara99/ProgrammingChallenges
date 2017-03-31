c3208ef94d1934699883455756c01aef3ab68007
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-31 17:26:13 +0100

    Solved another problem

--- UVA Online Judge/problem11456.cpp
+++ UVA Online Judge/problem11456.cpp
@@ -1,33 +1,23 @@
 #include <cstdio>
+int T, N, A[2001], LIS[2001], LDS[2001];
 
 int main() {
-
-    int T, N, A[2001], DP[2001][2];
-    
     scanf("%d", &T);
     while (T--) {
         scanf("%d", &N);
         int maxAns = 0;
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; ++i) {
             scanf("%d", &A[i]);
-            DP[i][0] = 0;
-            DP[i][1] = 0;
-            for (int j = 0; j < i; j++) {
-                if (A[j] >= A[i] && DP[j][0] > DP[i][0]) {
-                    DP[i][0] = DP[j][0];
-                }
-                if (A[j] <= A[i] && DP[j][1] > DP[i][1]) {
-                    DP[i][1] = DP[j][1];
-                }
+            LIS[i] = LDS[i] = 1;
+        }
+        for (int i = N-1; i >= 0; --i) {
+            for (int j = i+1; j < N; ++j) {
+                if (A[j] > A[i] && LIS[j] >= LIS[i]) LIS[i] = LIS[j]+1;
+                if (A[j] < A[i] && LDS[j] >= LDS[i]) LDS[i] = LDS[j]+1;
             }
-            DP[i][0]++;
-            DP[i][1]++;
-            if (DP[i][0] > maxAns) maxAns = DP[i][0];
-            if (DP[i][1] > maxAns) maxAns = DP[i][1];
+            if (LIS[i]+LDS[i] > maxAns) maxAns = LIS[i]+LDS[i]-1;
         }
         printf("%d\n", maxAns);
     }
-
-
     return 0;
 }
