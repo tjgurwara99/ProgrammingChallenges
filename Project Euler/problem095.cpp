3d3db7f80d608c25df1da223aef27dfa6e328c6a
Basim Khajwal <basimkhajwal@gmail.com>
2017-08-02 16:43:45 +0100

    Solved another Project Euler problem

--- Project Euler/problem095.cpp
+++ Project Euler/problem095.cpp
@@ -1,44 +1,34 @@
+/* Correct answer in 0.2s */
 #include <cstdio>
-using namespace std;
-
 #define LIM 1000000
-#define ll long long
 
-int nextNum[LIM+1];
-int seen[LIM+1];
-
-inline int divisorSum(int n) {
-    int total = 1;
-    for (int i = 2; i*i <= n; i++) {
-        if (n % i == 0) {
-            total += i;
-            if (i*i != n) total += n/i;
-        }
-    }
-    return total;
-}
+int nextNum[LIM+1], seen[LIM+1];
 
 int main() {
-    for (int i = 1; i <= LIM; i++) nextNum[i] = divisorSum(i);
 
-    int maxChainLength = 0;
+    nextNum[1] = 1;
+    for (int d = 1; d+d <= LIM; d++) {
+        for (int k = d+d; k <= LIM; k+=d) nextNum[k] += d;
+    }
+
+    int maxChainLength = 0, maxAns = -1;
 
     for (int i = 1; i <= LIM; i++) {
-        if (seen[i] != 0) continue;
-        int num = i, chainLength = i;
+        int num = i, chainLength = 0;
 
-        while (num <= LIM && seen[num] < i) {
-            seen[num] = chainLength++;
+        while (num <= LIM && seen[num] != i) {
+            chainLength++;
+            seen[num] = i;
             num = nextNum[num];
         }
-        if (num > LIM) break;
 
-        int l = chainLength - seen[num];
-        if (l > maxChainLength) {
-            maxChainLength = l;
-            printf("%d: %d\n", num, l);
+        if (num == i && chainLength > maxChainLength) {
+            maxChainLength = chainLength;
+            maxAns = i;
         }
     }
+
+    printf("%d: %d\n", maxAns, maxChainLength);
 
     return 0;
 }
