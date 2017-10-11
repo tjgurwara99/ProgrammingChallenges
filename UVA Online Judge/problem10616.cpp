1cd62e3412955a7fe64064da11571cdac8eb8cb3
Basim Khajwal <basimkhajwal@gmail.com>
2017-10-11 12:23:30 +0000

    Updated program, not working yet

--- UVA Online Judge/problem10616.cpp
+++ UVA Online Judge/problem10616.cpp
@@ -6,17 +6,27 @@ int main() {
     long long DP[11][200][21][20]; 
 
     while (scanf("%d %d", &N, &Q), N & Q) {
+        
+        for (int m = 0; m < 11; m++) {
+            for (int i = 0; i < N; i++) {
+                for (int d = 0; d < 21; d++) {
+                    for (int r = 0; r < d; r++) {
+                        DP[m][i][d][r] = 0;
+                    }
+                }
+            }
+        }
+        
         for (int i = 0; i < N; i++) {
             scanf("%d", &A[i]);
-            for (int d = 1; d <= 20; d++) DP[1][i][d][0] = (A[i] % d) ? 0 : 1;
+            for (int d = 1; d <= 20; d++) DP[1][i][d][A[i]%d] = 1;
         }
 
         for (int m = 2; m <= 10; m++) {
             for (int i = 0; i <= N-m; i++) {
                 for (int d = 1; d <= 20; d++) {
                     for (int r = 0; r < d; r++) {
-                        int c = (d-r+d-(A[i]%d))%d;
-                        DP[m][i][d][r] = 0;
+                        int c = (d-((r+A[i])%d))%d;
                         for (int j = i+1; j <= N-m+1; j++) {
                             DP[m][i][d][r] += DP[m-1][j][d][c];
                         }
@@ -30,7 +40,7 @@ int main() {
             int D, M;
             scanf("%d %d", &D, &M);
             long long ans = 0;
-            for (int i = 0; i < N; i++) ans += DP[M][i][D][0];
+            for (int i = 0; i <= N-M; i++) ans += DP[M][i][D][0];
             printf("QUERY %d: %lld\n", q+1, ans);
         }
     }
