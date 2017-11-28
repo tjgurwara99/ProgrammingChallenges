9087583f3f9d07064c90be63480070caf11505e3
Basim Khajwal <basimkhajwal@gmail.com>
2017-11-28 09:29:48 +0000

    Fixed overflow problem which led to me losing this task :(

--- Code Forces/448 Div2/b.cpp
+++ Code Forces/448 Div2/b.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 #define MAXN 123456
+#define ll long long
 
-int N, X, K, A[MAXN];
+int N;
+ll X, K, A[MAXN];
 
 int main() {
     
@@ -15,14 +17,14 @@ int main() {
     sort(A, A+N);
     
     int lastSame = 0;
-    long long total = 0;
+    ll total = 0;
     
     for (int i = 0; i < N; i++) {
         if (i && A[i] != A[i-1]) lastSame = i;
         
         int diff = (X - (A[i]%X)) % X;
-        int lower = K ? (A[i] + diff + (K-1)*X) : A[i];
-        int upper = A[i] + diff + K*X; // exclusive upper bound
+        ll lower = K ? (A[i] + diff + (K-1)*X) : A[i];
+        ll upper = A[i] + diff + K*X; // exclusive upper bound
         
         int a = (int)(lower_bound(A+lastSame, A+N, lower) - A);
         int b = (int)(lower_bound(A+lastSame, A+N, upper) - A);
