56d0b01b0aa121613c8f4f6ad75445b96d0d821b
Basim Khajwal <basimkhajwal@gmail.com>
2017-12-15 11:46:45 +0000

    Did some BIO solutions

---
+++ BIO Solutions/2016/round1/q1.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+int main() {
+    int l = 1, m = 0, r = 0, s = 1, a, b;
+    char S[20];
+    scanf("%s", S);
+    
+    for (int i = 0; S[i]; i++) {
+        if (S[i] == 'L') {
+            l = a;
+            m = b;
+        } else {
+            r = a;
+            s = b;
+        }
+        a = l+r;
+        b = m+s;
+    }
+    
+    printf("%d / %d\n", a, b);
+    return 0;
+}
