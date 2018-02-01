a15e625f094462230f1f6574147bfc5d81e0c0f7
Basim Khajwal <basimkhajwal@gmail.com>
2018-02-01 11:06:23 +0000

    Fixed solution

--- Code Forces/914/c.cpp
+++ Code Forces/914/c.cpp
@@ -18,11 +18,6 @@ int main() {
     cin >> N >> K;
     int M = (int)N.size();
     
-    if (K == 0) {
-        cout << 1 << endl;
-        return 0;
-    }
-    
     for (int i = 0; i <= 1000; i++) {
         NCR[i][0] = NCR[i][i] = 1;
         for (int j = 1; j < i; j++) NCR[i][j] = (NCR[i-1][j] + NCR[i-1][j-1]) % MOD;
@@ -36,13 +31,18 @@ int main() {
             }
         } else {
             for (int b = 1; i+b <= M; b++) {
-                bitDP[i][b] = (bitDP[i+1][b-1] + NCR[M-1-i][b]);
+                bitDP[i][b] = (bitDP[i+1][b-1] + NCR[M-1-i][b]) % MOD;
             }
         }
     }
     
+    if (K == 0) {
+        cout << 1 << endl;
+        return 0;
+    }
+    
     if (K == 1) {
-        cout << bitDP[0][1] << endl;
+        cout << bitDP[0][1]-1 << endl;
         return 0;
     }
     
