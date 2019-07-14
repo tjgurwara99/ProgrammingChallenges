407dc35a60d01c411f3ea0e602dcd21ec02de66c
basimkhajwal <basimkhajwal@gmail.com>
2019-07-14 12:39:21 +0100

    Some edits

--- Code Forces/1190/d.cpp
+++ Code Forces/1190/d.cpp
@@ -14,20 +14,56 @@ using namespace std;
 int N;
 pair<int,int> P[MAXN];
 
+vector<int> xs;
+int xCount[MAXN];
+map<int, int> xIndex;
+
+#define LSB(x) ((x)&(-(x)))
+
+void add(int x) {
+  int i = xIndex[x];
+  for (i < MAXN; i += LSB(i)) xCount[i]++;
+}
+
+int get(int i) {
+  int total = 0;
+  for (; i; i -= LSB(i)) total += xCount[i];
+  return total;
+}
+
+int getRange(int x1, int x2) {
+  int i = xIndex[x1], j = xIndex[x2];
+  return get(j)-get(i-1);
+}
+
 int main() {
 
   cin >> N;
-  for (int i = 0; i < N; i++) cin >> P[i].second >> P[i].first;
+  for (int i = 0; i < N; i++) {
+    cin >> P[i].second >> P[i].first;
+    xs.push_back(P[i].second);
+  }
+
+  sort(xs.begin(), xs.end());
+
+  int distintX = 1;
+  for (int i = 1; i < N; i++) {
+    if (xs[i] != xs[distinctX-1]) xs[distinctX++] = xs[i];
+  }
+  for (int i = 0; i < distinctX; i++) xIndex[xs[i]] = i+1;
 
   sort(P, P+N);
 
-  set<int> xs;
   ll ans = 0;
   for (int i = N-1; i >= 0;) {
     int j = i;
     for (;j >= 0 && P[j].first == P[i].first; j--) {
-      xs.insert(P[j].second);
+      add(P[j].second);
+      if (j < i) {
+
+      }
     }
+
     ll n = (ll)xs.size();
     ans += n*(n+1)/2;
     i = j;
